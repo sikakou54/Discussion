6a2e63e9ebbdcc762fecf4fa183857238e47e643
SasakiKouhei <sikakou0504@outlook.jp>
2022-12-02 11:59:22 +0900

    first commit

--- define/define.js
+++ define/define.js
@@ -1,53 +1,53 @@
 export const actions = {
     state: {
-        select: 0x00,
-        join: 0x01,
-        standby: 0x02,
-        ready: 0x03,
-        online: 0x04,
-        finish: 0x05,
-        vote: 0x06,
-        votingDone: 0x07,
-        result: 0x08
+        select: 1,
+        join: 2,
+        standby: 3,
+        ready: 4,
+        online: 5,
+        finish: 6,
+        vote: 7,
+        votingDone: 8,
+        result: 9
     },
-    changeCurrentTime: 0x10,
-    changeSocketId: 0x20,
-    attendees: 0x30,
-    start: 0x40,
-    timeout: 0x50,
-    vote: 0x60,
-    changeMeetingEventName: 0x70,
-    changeWebsocketStatus: 0x80,
-    changeDiscusionStatus: 0x90
+    changeCurrentTime: 10,
+    changeSocketId: 11,
+    attendees: 12,
+    start: 13,
+    timeout: 14,
+    vote: 15,
+    changeMeetingEventName: 16,
+    changeWebsocketStatus: 17,
+    changeDiscusionStatus: 18
 };
 
 export const discusionStatus = {
-    websocketDisconnect: 0x00,
-    discussionJoinFailed: 0x01,
-    discussionFailed: 0x02,
-    discussionStartFailed: 0x03,
-    discussionResultShow: 0x04,
-    returnPosts: 0x05,
-    websocketError: 0x06,
+    websocketDisconnect: 1,
+    discussionJoinFailed: 2,
+    discussionFailed: 3,
+    discussionStartFailed: 4,
+    discussionResultShow: 5,
+    returnPosts: 6,
+    websocketError: 7,
 };
 
 export const websocketStatus = {
-    open: 0x00,
-    close: 0x01
+    open: 1,
+    close: 2
 };
 
 export const discussionErrorMsg = {
-    websocketDisconnect: '通信が切断されました＿|￣|○',
-    discussionJoinFailed: '討論の参加に失敗しました＿|￣|○',
-    discussionFailed: '討論が異常終了しました＿|￣|○',
-    discussionStartFailed: '討論の参加に失敗しました＿|￣|○',
-    websocketError: '通信エラーが発生しました。＿|￣|○',
+    websocketDisconnect: '通信が切断されました',
+    discussionJoinFailed: '討論の参加に失敗しました',
+    discussionFailed: '討論が異常終了しました',
+    discussionStartFailed: '討論の参加に失敗しました',
+    websocketError: '通信エラーが発生しました',
 }
 
 export const discussionErrorCode = {
-    websocketDisconnect: 0x00,
-    discussionJoinFailed: 0x01,
-    discussionFailed: 0x02,
-    discussionStartFailed: 0x03,
-    websocketError: 0x04,
+    websocketDisconnect: 1,
+    discussionJoinFailed: 2,
+    discussionFailed: 3,
+    discussionStartFailed: 4,
+    websocketError: 5,
 }
