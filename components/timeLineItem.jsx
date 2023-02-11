7ae98083b08e19ec481684f892ef7ba9cd922285
SasakiKouhei <sikakou0504@outlook.jp>
2023-02-11 13:55:39 +0900

    first commit

--- components/timeLineItem.jsx
+++ components/timeLineItem.jsx
@@ -5,7 +5,7 @@ import styles from '../styles/TimeLineItem.module.css';
 import Image from "next/image";
 import user from '../public/user.svg';
 
-export default function TimeLineItem({ postId, progress, userId, title, positive, negative, watchers, createAt, onClick }) {
+export default function TimeLineItem({ postId, progress, title, positive, negative, watchers, createAt, onClick }) {
 
     return (
         <div className={styles.container} onClick={() => { onClick(postId) }} >
@@ -19,15 +19,31 @@ export default function TimeLineItem({ postId, progress, userId, title, positive
                 <div className={styles.title}>{title}</div>
                 <div className={styles.discussionLabelContainer}>
                     <div className={styles.discussionLabelItems}>
-                        <DiscussionLabel>{positive.text} </DiscussionLabel>
+                        <DiscussionLabel>
+                            <div className={styles.attendessContainer}>
+                                {
+                                    positive.userId === 'none'
+                                        ? <>{positive.text}</>
+                                        : <><div className={styles.userIcon}>< Image src={user} /></div><div className={styles.attendessText}>{positive.text}</div></>
+                                }
+                            </div>
+                        </DiscussionLabel>
                     </div>
                     <div className={styles.discussionLabelItems}>
-                        <DiscussionLabel>{negative.text} </DiscussionLabel>
+                        <DiscussionLabel>
+                            <div className={styles.attendessContainer}>
+                                {
+                                    negative.userId === 'none'
+                                        ? <>{negative.text}</>
+                                        : <><div className={styles.userIcon}>< Image src={user} /></div><div className={styles.attendessText}>{negative.text}</div></>
+                                }
+                            </div>
+                        </DiscussionLabel>
                     </div>
                     <div className={styles.discussionLabelItemsWatcher}>
                         <DiscussionLabel>
                             <div className={styles.watcherCount}>
-                                <div className={styles.watcherIcon}>< Image src={user} /></div>
+                                <div className={styles.userIcon}>< Image src={user} /></div>
                                 <div className={styles.watcherText}>{watchers.length}</div>
                             </div>
                         </DiscussionLabel>
