095e39000fff112dc523c11e4edc32c2a8b3b3b1
SasakiKouhei <sikakou0504@outlook.jp>
2023-02-11 16:57:50 +0900

    first commit

--- components/button.jsx
+++ components/button.jsx
@@ -1,7 +1,7 @@
 import style from '../styles/Button.module.css';
 
-export default function Button({ text, onClick }) {
+export default function Button({ text, disable, onClick }) {
     return (
-        <button className={style.container} onClick={onClick}>{text}</button>
+        <button className={style.container} disabled={disable} onClick={onClick}>{text}</button>
     );
 }
--- pages/post/index.jsx
+++ pages/post/index.jsx
@@ -13,23 +13,18 @@ export default function Post({ userId }) {
     const [negativeText, setNegativeText] = useState('');
 
     async function onPost() {
-
-        if ('' !== title && '' !== detail && '' !== positiveText && '' !== negativeText) {
-
-            apiFetchPost(process.env.awsApiGatewayHttpApiEndPoint + '/setDiscussion', {
-                body: JSON.stringify({
-                    country: 'jpn',
-                    userId,
-                    title,
-                    detail,
-                    positiveText,
-                    negativeText
-                })
-            }).then(() => {
-                Router.push('/posts');
-            });
-
-        }
+        apiFetchPost(process.env.awsApiGatewayHttpApiEndPoint + '/setDiscussion', {
+            body: JSON.stringify({
+                country: 'jpn',
+                userId,
+                title,
+                detail,
+                positiveText,
+                negativeText
+            })
+        }).then(() => {
+            Router.push('/posts');
+        });
     }
 
     function onCancel() {
@@ -39,19 +34,38 @@ export default function Post({ userId }) {
     return (
         <Layout userId={userId} title={'Post'}>
             <div className={style.container}>
-                <div><input className={style.title} type='text' placeholder='タイトル' required onChange={(e) => { setTitle(e.target.value) }} /></div>
+                <div><input className={style.title} type='text' placeholder='朝食はパン？ごはん？' required onChange={(e) => { setTitle(e.target.value) }} /></div>
+                {title.length > 30 && <div className={style.errorText}>30文字以内で入力してください</div>}
+                {title.length === 0 && <div className={style.errorText}>タイトルを入力してください</div>}
+
                 <div className={style.frame}>
                     <div className={style.secsionName}>討論</div>
-                    <div><input className={style.discussion} type='text' placeholder='1' required onChange={(e) => { setPositiveText(e.target.value) }} /></div>
-                    <div><input className={style.discussion} type='text' placeholder='2' required onChange={(e) => { setNegativeText(e.target.value) }} /></div>
+                    <div><input className={style.discussion} type='text' placeholder='ぱん' required onChange={(e) => { setPositiveText(e.target.value) }} /></div>
+                    {positiveText.length > 20 && <div className={style.errorText}>20文字以内で入力してください</div>}
+                    {positiveText.length === 0 && <div className={style.errorText}>討論を入力してください</div>}
+
+                    <div><input className={style.discussion} type='text' placeholder='ごはん' required onChange={(e) => { setNegativeText(e.target.value) }} /></div>
+                    {negativeText.length > 20 && <div className={style.errorText}>20文字以内で入力してください</div>}
+                    {negativeText.length === 0 && <div className={style.errorText}>討論を入力してください</div>}
+
                 </div>
                 <div className={style.frame}>
                     <div className={style.secsionName}>概要</div>
                     <textarea className={style.oval} id="story" name="story" rows="5" cols="33" required onChange={(e) => { setDetail(e.target.value) }} />
+                    {detail.length > 140 && <div className={style.errorText}>140文字以内で入力してください</div>}
+                    {detail.length === 0 && <div className={style.errorText}>概要を入力してください</div>}
+
                 </div>
                 <div className={style.buttonArea}>
                     <div className={style.buttonAreaItem}><Button onClick={onCancel} text={'キャンセル'} /></div>
-                    <div className={style.buttonAreaItem}><Button onClick={onPost} text={'投稿する'} /></div>
+                    {
+                        (title.length > 0 && title.length <= 30) &&
+                            (positiveText.length > 0 && positiveText.length <= 20) &&
+                            (negativeText.length > 0 && negativeText.length <= 20) &&
+                            (detail.length > 0 && detail.length <= 140)
+                            ? <div className={style.buttonAreaItem}><Button disable={false} onClick={onPost} text={'投稿する'} /></div>
+                            : <div className={style.buttonAreaItem}><Button disaïble={true} onClick={onPost} text={'投稿する'} /></div>
+                    }ï
                 </div>
             </div>
         </Layout>
