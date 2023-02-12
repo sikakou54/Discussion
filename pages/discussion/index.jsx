d1899371ffe3499b6c284e97cf47e8a090c8c0db
SasakiKouhei <sikakou0504@outlook.jp>
2023-02-12 10:33:54 +0900

    first commit

--- pages/discussion/index.jsx
+++ pages/discussion/index.jsx
@@ -6,49 +6,48 @@ import { ThemeProvider } from 'styled-components';
 import Discussion from '../../components/discussion';
 import { apiFetchGet } from '../../api/utils';
 import Layout from '../../components/layout';
+import { useEffect, useState } from 'react';
 
-export default function DiscussionManager({ discussion, userId }) {
+export default function DiscussionManager({ postId, userId }) {
 
-    return (
-        <Layout userId={userId} title={'Discussion'}>
-            < ThemeProvider theme={lightTheme} >
-                <MeetingProvider>
-                    <Discussion discussion={discussion} userId={userId} />
-                </MeetingProvider>
-            </ThemeProvider >
-        </Layout>
-    );
+    const [discussion, setDiscussion] = useState(null);
+
+    useEffect(() => {
+
+        apiFetchGet(process.env.awsApiGatewayHttpApiEndPoint + '/getDiscussion/' + 'jpn' + '/' + postId).then((response) => {
+            console.log(response);
+            setDiscussion(response.data);
+        });
+
+    }, []);
+
+    if (null !== discussion) {
+
+        return (
+            <Layout userId={userId} title={'Discussion'}>
+                < ThemeProvider theme={lightTheme} >
+                    <MeetingProvider>
+                        <Discussion discussion={discussion} userId={userId} />
+                    </MeetingProvider>
+                </ThemeProvider >
+            </Layout>
+        );
+
+    } else {
+        return null;
+    }
 }
 
 //SSR
 export async function getServerSideProps(ctx) {
 
     const { postId } = ctx.query;
-    const res = await apiFetchGet(process.env.awsApiGatewayHttpApiEndPoint + '/getDiscussion/' + 'jpn' + '/' + postId, {
-        /**
-        headers: {
-            Authorization: process.env.jwt
+
+    return {
+        props: {
+            postId,
+            userId: 'a18c3444-56c3-43c3-a34b-41263fd64d35'
         }
-         */
-    });
-
-    if (res.result) {
-
-        return {
-            props: {
-                discussion: res.data,
-                userId: 'a18c3444-56c3-43c3-a34b-41263fd64d35'
-            }
-        };
-
-    } else {
-
-        return {
-            redirect: {
-                destination: '/posts',
-                permanent: false
-            }
-        }
-    }
+    };
 
 }
