40b8856ee4ace86660c6e0e58e95eaa4228d8773
SasakiKouhei <sikakou0504@outlook.jp>
2023-02-18 16:27:03 +0900

    first commit

--- pages/post/index.jsx
+++ pages/post/index.jsx
@@ -34,28 +34,31 @@ export default function Post({ userId }) {
     return (
         <Layout title={'Post'}>
             <div className={style.container}>
-                <div><input className={style.title} type='text' placeholder='朝食はパン？ごはん？' required onChange={(e) => { setTitle(e.target.value) }} /></div>
-                {title.length > 30 && <div className={style.errorText}>30文字以内で入力してください</div>}
-                {title.length === 0 && <div className={style.errorText}>タイトルを入力してください</div>}
+
+                <div className={style.frame}>
+                    <div className={style.secsionName}>議題</div>
+                    <div><input className={style.title} type='text' placeholder='朝食はパン？ごはん？' required onChange={(e) => { setTitle(e.target.value) }} /></div>
+                    {title.length > 30 && <div className={style.errorText}>30文字以内で入力してください</div>}
+                    {title.length === 0 && <div className={style.errorText}>タイトルを入力してください</div>}
+                </div>
 
                 <div className={style.frame}>
                     <div className={style.secsionName}>討論</div>
                     <div><input className={style.discussion} type='text' placeholder='ぱん' required onChange={(e) => { setPositiveText(e.target.value) }} /></div>
                     {positiveText.length > 20 && <div className={style.errorText}>20文字以内で入力してください</div>}
                     {positiveText.length === 0 && <div className={style.errorText}>討論を入力してください</div>}
-
                     <div><input className={style.discussion} type='text' placeholder='ごはん' required onChange={(e) => { setNegativeText(e.target.value) }} /></div>
                     {negativeText.length > 20 && <div className={style.errorText}>20文字以内で入力してください</div>}
                     {negativeText.length === 0 && <div className={style.errorText}>討論を入力してください</div>}
-
                 </div>
+
                 <div className={style.frame}>
                     <div className={style.secsionName}>概要</div>
                     <textarea className={style.oval} id="story" name="story" rows="5" cols="33" required onChange={(e) => { setDetail(e.target.value) }} />
                     {detail.length > 140 && <div className={style.errorText}>140文字以内で入力してください</div>}
                     {detail.length === 0 && <div className={style.errorText}>概要を入力してください</div>}
-
                 </div>
+
                 <div className={style.buttonArea}>
                     <button className={style.cancel} onClick={onCancel}>キャンセル</button>
                     {
