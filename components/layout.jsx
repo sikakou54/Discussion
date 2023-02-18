bc80f9848a59c4698d1178a2255c45380e60befe
SasakiKouhei <sikakou0504@outlook.jp>
2023-02-19 01:21:09 +0900

    first commit

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,13 +1,14 @@
 import Footer from './footer';
 import Header from './header';
+import styles from '../styles/Layout.module.css';
 
 export default function Layout({ children, title }) {
 
     return (
-        <>
+        <div className={styles.container}>
             <Header title={title} />
-            {children}
-            <Footer />
-        </>
+            <div className={styles.main}>{children}</div>
+            <div className={styles.footer}><Footer /></div>
+        </div>
     );
 }
