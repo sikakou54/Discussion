2098315a03fd8fe51871bc4ecaf969553dbe4623
SasakiKouhei <sikakou0504@outlook.jp>
2023-02-21 23:48:13 +0900

    first commit

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,20 +1,18 @@
 import styles from '../styles/Home.module.css'
-import Layout from '../components/layout';
 import Router from 'next/router';
 import Image from 'next/image';
 import bgimage from '../public/bgimage.png';
+import Footer from '../components/footer';
 
 export default function Home() {
 
   return (
     <div className={styles.container}>
-
       <div className={styles.top}>
         <div className={styles.bgImage}>
           <Image src={bgimage} />
         </div>
       </div>
-
       <div className={styles.second}>
         <div className={styles.aboutText}>
           <div className={styles.about}>What’s TalkUp？</div>
@@ -23,47 +21,40 @@ export default function Home() {
         </div>
         <button className={styles.startButton} onClick={() => Router.push('/posts')}>はじめる</button>
       </div>
-
       <div className={styles.third}>
         <div className={styles.bar}>遊び方</div>
-
         <div className={styles.section}>
           <div className={styles.label}>
             <div>気になるROOMを選びます</div>
           </div>
           <div className={styles.screenImange}></div>
         </div>
-
         <div className={styles.section}>
           <div className={styles.label}>
             <div>ROOMに参加します</div>
           </div>
           <div className={styles.screenImange}></div>
         </div>
-
         <div className={styles.section}>
           <div className={styles.label}>
             <div>参加者がそろったら討論開始</div>
           </div>
           <div className={styles.screenImange}></div>
         </div>
-
         <div className={styles.section}>
           <div className={styles.label}>
             <div>投票タイム</div>
           </div>
           <div className={styles.screenImange}></div>
         </div>
-
         <div className={styles.section}>
           <div className={styles.label}>
             <div>結果発表</div>
           </div>
           <div className={styles.screenImange}></div>
         </div>
-
       </div>
-
+      <div className={styles.footer}><Footer /></div>
     </div>
   );
 }
