0def57164464bc02ae873b590524d5b376460cf5
SasakiKouhei <sikakou0504@outlook.jp>
2023-02-21 18:59:37 +0900

    first commit

--- pages/discussion/index.jsx
+++ pages/discussion/index.jsx
@@ -6,52 +6,42 @@ import { ThemeProvider } from 'styled-components';
 import Discussion from '../../components/discussion';
 import { apiFetchGet } from '../../api/utils';
 import Layout from '../../components/layout';
-import { useEffect, useState } from 'react';
-import Router from 'next/router';
 
-export default function DiscussionManager({ postId, userId }) {
+export default function DiscussionManager({ userId, discussion }) {
 
-    const [discussion, setDiscussion] = useState(null);
-
-    useEffect(() => {
-
-        apiFetchGet(process.env.awsApiGatewayHttpApiEndPoint + '/getDiscussion/' + 'jpn' + '/' + postId).then((response) => {
-            if (200 == response.statusCode) {
-                setDiscussion(response.data);
-            } else {
-                Router.push('/posts');
-            }
-        });
-
-    }, []);
-
-    if (null !== discussion) {
-
-        return (
-            <Layout title={'Discussion'}>
-                < ThemeProvider theme={lightTheme} >
-                    <MeetingProvider>
-                        <Discussion discussion={discussion} userId={userId} />
-                    </MeetingProvider>
-                </ThemeProvider >
-            </Layout>
-        );
-
-    } else {
-        return null;
-    }
+    return (
+        <Layout title={'Discussion'}>
+            < ThemeProvider theme={lightTheme} >
+                <MeetingProvider>
+                    <Discussion discussion={discussion} userId={userId} />
+                </MeetingProvider>
+            </ThemeProvider >
+        </Layout>
+    );
 }
 
 //SSR
-export async function getServerSideProps(ctx) {
+export async function getServerSideProps(context) {
 
-    const { postId } = ctx.query;
+    const { postId } = context.query;
+    const response = await apiFetchGet(process.env.awsApiGatewayHttpApiEndPoint + '/getDiscussion/' + 'jpn' + '/' + postId);
 
-    return {
-        props: {
-            postId,
-            userId: 'a18c3444-56c3-43c3-a34b-41263fd64d35'
-        }
-    };
+    if (200 == response.statusCode) {
 
+        return {
+            props: {
+                userId: 'a18c3444-56c3-43c3-a34b-41263fd64d35',
+                discussion: response.data
+            }
+        };
+
+    } else {
+
+        return {
+            redirect: {
+                permanent: false,
+                destination: '/posts',
+            },
+        };
+    }
 }
