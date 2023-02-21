681955b548591c0765577ec509b84b613c327ce6
SasakiKouhei <sikakou0504@outlook.jp>
2023-02-21 23:21:34 +0900

    first commit

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,61 +1,66 @@
 import styles from '../styles/Home.module.css'
 import Layout from '../components/layout';
 import Router from 'next/router';
+import Image from 'next/image';
+import bgimage from '../public/bgimage.png';
 
 export default function Home() {
 
   return (
-    <Layout userId={undefined} title={'Home'}>
-      <div className={styles.container}>
+    <div className={styles.container}>
 
-        <div className={styles.top}>
-          <div className={styles.titleSecssion}>
-            <div className={styles.appNameSction} >匿名討論アプリ</div>
-            <div className={styles.appName}>TalkUp</div>
-          </div>
-          <button className={styles.startButton} onClick={() => Router.push('/posts')}>はじめる</button>
+      <div className={styles.top}>
+        <div className={styles.bgImage}>
+          <Image src={bgimage} />
         </div>
-
-        <div className={styles.bar}>
-          <div>遊び方</div>
-        </div>
-
-        <div className={styles.section}>
-          <div className={styles.label}>
-            <div>気になるROOMを選びます</div>
-          </div>
-          <div className={styles.screenImange}></div>
-        </div>
-
-        <div className={styles.section}>
-          <div className={styles.label}>
-            <div>ROOMに参加します</div>
-          </div>
-          <div className={styles.screenImange}></div>
-        </div>
-
-        <div className={styles.section}>
-          <div className={styles.label}>
-            <div>参加者がそろったら討論開始</div>
-          </div>
-          <div className={styles.screenImange}></div>
-        </div>
-
-        <div className={styles.section}>
-          <div className={styles.label}>
-            <div>投票タイム</div>
-          </div>
-          <div className={styles.screenImange}></div>
-        </div>
-
-        <div className={styles.section}>
-          <div className={styles.label}>
-            <div>結果発表</div>
-          </div>
-          <div className={styles.screenImange}></div>
-        </div>
-
       </div>
-    </Layout>
+
+      <div className={styles.second}>
+        <div className={styles.aboutText}>
+          <div className={styles.about}>What's TalkUp？</div>
+          <div className={styles.detail}>TalkUpは匿名の討論アプリです</div>
+          <div className={styles.detail}>TalkUpであなたのTalk力を測ってみませんか？</div>
+        </div>
+        <button className={styles.startButton} onClick={() => Router.push('/posts')}>はじめる</button>
+      </div>
+
+      <div className={styles.bar}>遊び方</div>
+
+      <div className={styles.section}>
+        <div className={styles.label}>
+          <div>気になるROOMを選びます</div>
+        </div>
+        <div className={styles.screenImange}></div>
+      </div>
+
+      <div className={styles.section}>
+        <div className={styles.label}>
+          <div>ROOMに参加します</div>
+        </div>
+        <div className={styles.screenImange}></div>
+      </div>
+
+      <div className={styles.section}>
+        <div className={styles.label}>
+          <div>参加者がそろったら討論開始</div>
+        </div>
+        <div className={styles.screenImange}></div>
+      </div>
+
+      <div className={styles.section}>
+        <div className={styles.label}>
+          <div>投票タイム</div>
+        </div>
+        <div className={styles.screenImange}></div>
+      </div>
+
+      <div className={styles.section}>
+        <div className={styles.label}>
+          <div>結果発表</div>
+        </div>
+        <div className={styles.screenImange}></div>
+      </div>
+
+    </div>
   );
 }
