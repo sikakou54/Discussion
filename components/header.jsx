4c40ae4580bf6a068d5240cbc85fb8a7606766b7
SasakiKouhei <sikakou0504@outlook.jp>
2023-03-05 00:26:41 +0900

    first commit

--- components/header.jsx
+++ components/header.jsx
@@ -10,6 +10,7 @@ export default function Header({ title }) {
         <header className={styles.container}>
             <Head>
                 <title>{title}</title>
+                <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0"></meta>
             </Head>
             <Link href="/">
                 <a className={styles.headerSectioin}>
