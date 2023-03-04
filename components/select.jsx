6fa0d809aa0be62ad391ac7f11e78e67b202e6bc
SasakiKouhei <sikakou0504@outlook.jp>
2023-03-04 16:24:06 +0900

    first commit

--- components/select.jsx
+++ components/select.jsx
@@ -17,63 +17,69 @@ export default function Select({ onJoin, attendees, title, detail }) {
             <div className={styles.title}>{title}</div>
             <textarea value={detail} className={styles.detail} readOnly />
             <div className={styles.selectItems}>
-                {
-                    'none' === attendees.positive.userId ?
-                        (
-                            selectedJoinType === 1 ?
-                                (
-                                    <div className={`${styles.selectItem} ${styles.selected}`} disabled={false}>
-                                        <Attendee userId={attendees.positive.userId}>{attendees.positive.text}</Attendee>
-                                    </div>
-                                ) : (
-                                    <div className={`${styles.selectItem} ${styles.enable}`} onClick={() => setSelectedJoinType(1)} disabled={false}>
-                                        <Attendee userId={attendees.positive.userId}>{attendees.positive.text}</Attendee>
-                                    </div>
-                                )
-                        ) : (
-                            <div className={`${styles.selectItem} ${styles.disable}`} onClick={() => setSelectedJoinType(1)} disabled={true}>
-                                <Attendee userId={attendees.positive.userId}>{attendees.positive.text}</Attendee>
-                            </div>
-                        )
-                }
-                {
-                    'none' === attendees.negative.userId ?
-                        (
-                            selectedJoinType === 2 ?
-                                (
-                                    <div className={`${styles.selectItem} ${styles.selected}`} disabled={false}>
-                                        <Attendee userId={attendees.negative.userId}>{attendees.negative.text}</Attendee>
-                                    </div>
-                                ) : (
-                                    <div className={`${styles.selectItem} ${styles.enable}`} onClick={() => setSelectedJoinType(2)} disabled={false}>
-                                        <Attendee userId={attendees.negative.userId}>{attendees.negative.text}</Attendee>
-                                    </div>
-                                )
-                        ) : (
-                            <div className={`${styles.selectItem} ${styles.disable}`} onClick={() => setSelectedJoinType(2)} disabled={true}>
-                                <Attendee userId={attendees.negative.userId}>{attendees.negative.text}</Attendee>
-                            </div>
-                        )
-                }
-                {
-                    100 > attendees.watchers.length ?
-                        (
-                            selectedJoinType === 3 ?
-                                (
-                                    <div className={`${styles.selectItem} ${styles.selected}`} disabled={false}>
-                                        <Attendee userId={'none'}>視聴する</Attendee>
-                                    </div>
-                                ) : (
-                                    <div className={`${styles.selectItem} ${styles.enable}`} onClick={() => setSelectedJoinType(3)} disabled={false}>
-                                        <Attendee userId={'none'}>視聴する</Attendee>
-                                    </div>
-                                )
-                        ) : (
-                            <div className={`${styles.selectItem} ${styles.disable}`} onClick={() => setSelectedJoinType(3)} disabled={true}>
-                                <Attendee userId={'none'}>視聴する</Attendee>
-                            </div>
-                        )
-                }
+                <div className={styles.selectItemBox}>
+                    {
+                        'none' === attendees.positive.userId ?
+                            (
+                                selectedJoinType === 1 ?
+                                    (
+                                        <div className={`${styles.selectItem} ${styles.selected}`} disabled={false}>
+                                            <Attendee userId={attendees.positive.userId}>{attendees.positive.text}</Attendee>
+                                        </div>
+                                    ) : (
+                                        <div className={`${styles.selectItem} ${styles.enable}`} onClick={() => setSelectedJoinType(1)} disabled={false}>
+                                            <Attendee userId={attendees.positive.userId}>{attendees.positive.text}</Attendee>
+                                        </div>
+                                    )
+                            ) : (
+                                <div className={`${styles.selectItem} ${styles.disable}`} onClick={() => setSelectedJoinType(1)} disabled={true}>
+                                    <Attendee userId={attendees.positive.userId}>{attendees.positive.text}</Attendee>
+                                </div>
+                            )
+                    }
+                </div>
+                <div className={styles.selectItemBox}>
+                    {
+                        'none' === attendees.negative.userId ?
+                            (
+                                selectedJoinType === 2 ?
+                                    (
+                                        <div className={`${styles.selectItem} ${styles.selected}`} disabled={false}>
+                                            <Attendee userId={attendees.negative.userId}>{attendees.negative.text}</Attendee>
+                                        </div>
+                                    ) : (
+                                        <div className={`${styles.selectItem} ${styles.enable}`} onClick={() => setSelectedJoinType(2)} disabled={false}>
+                                            <Attendee userId={attendees.negative.userId}>{attendees.negative.text}</Attendee>
+                                        </div>
+                                    )
+                            ) : (
+                                <div className={`${styles.selectItem} ${styles.disable}`} onClick={() => setSelectedJoinType(2)} disabled={true}>
+                                    <Attendee userId={attendees.negative.userId}>{attendees.negative.text}</Attendee>
+                                </div>
+                            )
+                    }
+                </div>
+                <div className={styles.selectItemBox}>
+                    {
+                        100 > attendees.watchers.length ?
+                            (
+                                selectedJoinType === 3 ?
+                                    (
+                                        <div className={`${styles.selectItem} ${styles.selected}`} disabled={false}>
+                                            <Attendee userId={'none'}>視聴する</Attendee>
+                                        </div>
+                                    ) : (
+                                        <div className={`${styles.selectItem} ${styles.enable}`} onClick={() => setSelectedJoinType(3)} disabled={false}>
+                                            <Attendee userId={'none'}>視聴する</Attendee>
+                                        </div>
+                                    )
+                            ) : (
+                                <div className={`${styles.selectItem} ${styles.disable}`} onClick={() => setSelectedJoinType(3)} disabled={true}>
+                                    <Attendee userId={'none'}>視聴する</Attendee>
+                                </div>
+                            )
+                    }
+                </div>
             </div>
             {
                 undefined === selectedJoinType
