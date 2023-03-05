8c51c96b87c80d1bdd1acd9e8c6c3195945b7782
SasakiKouhei <sikakou0504@outlook.jp>
2023-03-05 17:45:55 +0900

    first commit

--- pages/index.jsx
+++ pages/index.jsx
@@ -65,7 +65,6 @@ export default function Home() {
           </form>
         </div>
       </div>
-
       <div className={styles.section}>
         <div className={styles.secssionItem}>
           <div className={styles.secssionText}><u>1.気になるROOMを選びます</u></div>
@@ -73,73 +72,61 @@ export default function Home() {
             <Image src={timeline} />
           </div>
         </div>
-
         <div className={styles.secssionItem}>
           <div className={styles.secssionText}><u>2.選択して「参加」をクリック！</u></div>
           <div className={styles.screenImange}>
             <Image src={select} />
           </div>
         </div>
-
         <div className={styles.secssionItem}>
           <div className={styles.secssionText}><u>3.ROOMに問い合わせを行います</u></div>
           <div className={styles.screenImange}>
             <Image src={join} />
           </div>
         </div>
-
         <div className={styles.secssionItem}>
           <div className={styles.secssionText}><u>4.参加者が集まるまで待機します</u></div>
           <div className={styles.screenImange}>
             <Image src={standby} />
           </div>
         </div>
-
         <div className={styles.secssionItem}>
           <div className={styles.secssionText}><u>5.参加者が集まったら準備が始まります</u></div>
           <div className={styles.screenImange}>
             <Image src={ready} />
           </div>
         </div>
-
         <div className={styles.secssionItem}>
           <div className={styles.secssionText}><u>6.討論が開始します(10分)</u></div>
           <div className={styles.screenImange}>
             <Image src={discussion} />
           </div>
         </div>
-
         <div className={styles.secssionItem}>
           <div className={styles.secssionText}><u>7.討論終了</u></div>
           <div className={styles.screenImange}>
             <Image src={finish} />
           </div>
         </div>
-
         <div className={styles.secssionItem}>
           <div className={styles.secssionText}><u>8.投票タイム(30秒)</u></div>
           <div className={styles.screenImange}>
             <Image src={vate} />
           </div>
         </div>
-
         <div className={styles.secssionItem}>
           <div className={styles.secssionText}><u>9.投票終了</u></div>
           <div className={styles.screenImange}>
             <Image src={voteFinish} />
           </div>
         </div>
-
         <div className={styles.secssionItem}>
           <div className={styles.secssionText}><u>10.結果発表</u></div>
           <div className={styles.screenImange}>
             <Image src={result} />
           </div>
         </div>
-
       </div>
-
-
       <div className={styles.footer}><Footer /></div>
     </div>
   );
