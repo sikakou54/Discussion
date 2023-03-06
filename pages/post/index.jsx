df7d8820ef5129fd82416f6127a2e9afc5c90d15
SasakiKouhei <sikakou0504@outlook.jp>
2023-03-06 16:51:06 +0900

    first commit

--- pages/post/index.jsx
+++ pages/post/index.jsx
@@ -4,6 +4,7 @@ import Layout from '../../components/layout';
 import style from '../../styles/Post.module.css';
 import Loding from '../../components/loading';
 import { discussionErrorCode } from '../../define/define';
+
 export default function Post() {
 
     const router = useRouter();
