bb4b965d17093da6184c3b1159c17fac999fb7b1
SasakiKouhei <sikakou0504@outlook.jp>
2023-03-08 22:50:00 +0900

    first commit

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script';
 
 export default function Document() {
     return (
@@ -14,6 +15,8 @@ export default function Document() {
                 <link rel="manifest" href="/site.webmanifest" />
                 <meta name="msapplication-TileColor" content="#da532c" />
                 <meta name="theme-color" content="#ffffff" />
+                <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6975386050212014"
+                    crossorigin="anonymous" />
             </Head>
             <body>
                 <Main />
