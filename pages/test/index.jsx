9c295602230d55212f6a807e86431f56ee903519
SasakiKouhei <sikakou0504@outlook.jp>
2023-03-11 20:31:11 +0900

    first commit

--- pages/test/index.jsx
+++ pages/test/index.jsx
@@ -1,17 +1,9 @@
-import { putRecored } from "../../api/kinesis";
 import Clock from "../../components/clock";
 
 export default function Test() {
 
-    async function click() {
-
-        const data = await putRecored();
-        console.log(data);
-    }
-
     return (
         <div>
-            <button onClick={click}>test</button>
             <Clock />
         </div>
     );
