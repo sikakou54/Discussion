489179ee92dbd41f91afe41670f013673b444235
SasakiKouhei <sikakou0504@outlook.jp>
2023-03-14 19:12:13 +0900

    first commit

--- pages/api/setDiscussion/index.js
+++ pages/api/setDiscussion/index.js
@@ -3,6 +3,5 @@ import { setDiscussion } from "../../../api/discussion";
 export default async function handler(req, res) {
     const { country, postId, userId, title, detail, positiveText, negativeText } = JSON.parse(req.body);
     const response = await setDiscussion(country, Number(postId), userId, title, detail, positiveText, negativeText);
-    console.log(response);
     res.status(200).json({ response });
 }
