8e8cc35c56982e9e9630a48d15adc663baf7c82d
SasakiKouhei <sikakou0504@outlook.jp>
2023-03-14 10:40:56 +0900

    first commit

--- components/select.jsx
+++ components/select.jsx
@@ -15,71 +15,67 @@ export default function Select({ onJoin, attendees, title, detail }) {
     return (
         <div className={styles.container}>
             <div className={styles.title}>{title}</div>
-            <textarea value={detail} className={styles.detail} readOnly />
+            <div className={styles.detailSession}>
+                <textarea value={detail} className={styles.detail} readOnly />
+            </div>
             <div className={styles.selectItems}>
-                <div className={styles.selectItem}>
-                    {
-                        'none' === attendees.positive.userId ?
-                            (
-                                selectedJoinType === 1 ?
-                                    (
-                                        <div className={`${styles.attendees} ${styles.selected}`} disabled={false}>
-                                            <Attendee userId={attendees.positive.userId}>{attendees.positive.text}</Attendee>
-                                        </div>
-                                    ) : (
-                                        <div className={`${styles.attendees} ${styles.enable}`} onClick={() => setSelectedJoinType(1)} disabled={false}>
-                                            <Attendee userId={attendees.positive.userId}>{attendees.positive.text}</Attendee>
-                                        </div>
-                                    )
-                            ) : (
-                                <div className={`${styles.attendees} ${styles.disable}`} onClick={() => setSelectedJoinType(1)} disabled={true}>
-                                    <Attendee userId={attendees.positive.userId}>{attendees.positive.text}</Attendee>
-                                </div>
-                            )
-                    }
-                </div>
-                <div className={styles.selectItem}>
-                    {
-                        'none' === attendees.negative.userId ?
-                            (
-                                selectedJoinType === 2 ?
-                                    (
-                                        <div className={`${styles.attendees} ${styles.selected}`} disabled={false}>
-                                            <Attendee userId={attendees.negative.userId}>{attendees.negative.text}</Attendee>
-                                        </div>
-                                    ) : (
-                                        <div className={`${styles.attendees} ${styles.enable}`} onClick={() => setSelectedJoinType(2)} disabled={false}>
-                                            <Attendee userId={attendees.negative.userId}>{attendees.negative.text}</Attendee>
-                                        </div>
-                                    )
-                            ) : (
-                                <div className={`${styles.attendees} ${styles.disable}`} onClick={() => setSelectedJoinType(2)} disabled={true}>
-                                    <Attendee userId={attendees.negative.userId}>{attendees.negative.text}</Attendee>
-                                </div>
-                            )
-                    }
-                </div>
-                <div className={styles.selectItem}>
-                    {
-                        100 > attendees.watchers.length ?
-                            (
-                                selectedJoinType === 3 ?
-                                    (
-                                        <div className={`${styles.attendees} ${styles.selected}`} disabled={false}>
-                                            <Attendee userId={'none'}>視聴する</Attendee>
-                                        </div>
-                                    ) : (
-                                        <div className={`${styles.attendees} ${styles.enable}`} onClick={() => setSelectedJoinType(3)} disabled={false}>
-                                            <Attendee userId={'none'}>視聴する</Attendee>
-                                        </div>
-                                    )
-                            ) : (
-                                <div className={`${styles.attendees} ${styles.disable}`} onClick={() => setSelectedJoinType(3)} disabled={true}>
-                                    <Attendee userId={'none'}>視聴する</Attendee>
-                                </div>
-                            )
-                    }
-                </div>
+                {
+                    'none' === attendees.positive.userId ?
+                        (
+                            selectedJoinType === 1 ?
+                                (
+                                    <div className={`${styles.attendees} ${styles.selected}`} disabled={false}>
+                                        <Attendee userId={attendees.positive.userId}>{attendees.positive.text}</Attendee>
+                                    </div>
+                                ) : (
+                                    <div className={`${styles.attendees} ${styles.enable}`} onClick={() => setSelectedJoinType(1)} disabled={false}>
+                                        <Attendee userId={attendees.positive.userId}>{attendees.positive.text}</Attendee>
+                                    </div>
+                                )
+                        ) : (
+                            <div className={`${styles.attendees} ${styles.disable}`} onClick={() => setSelectedJoinType(1)} disabled={true}>
+                                <Attendee userId={attendees.positive.userId}>{attendees.positive.text}</Attendee>
+                            </div>
+                        )
+                }
+                {
+                    'none' === attendees.negative.userId ?
+                        (
+                            selectedJoinType === 2 ?
+                                (
+                                    <div className={`${styles.attendees} ${styles.selected}`} disabled={false}>
+                                        <Attendee userId={attendees.negative.userId}>{attendees.negative.text}</Attendee>
+                                    </div>
+                                ) : (
+                                    <div className={`${styles.attendees} ${styles.enable}`} onClick={() => setSelectedJoinType(2)} disabled={false}>
+                                        <Attendee userId={attendees.negative.userId}>{attendees.negative.text}</Attendee>
+                                    </div>
+                                )
+                        ) : (
+                            <div className={`${styles.attendees} ${styles.disable}`} onClick={() => setSelectedJoinType(2)} disabled={true}>
+                                <Attendee userId={attendees.negative.userId}>{attendees.negative.text}</Attendee>
+                            </div>
+                        )
+                }
+                {
+                    100 > attendees.watchers.length ?
+                        (
+                            selectedJoinType === 3 ?
+                                (
+                                    <div className={`${styles.attendees} ${styles.selected}`} disabled={false}>
+                                        <Attendee userId={'none'}>視聴する</Attendee>
+                                    </div>
+                                ) : (
+                                    <div className={`${styles.attendees} ${styles.enable}`} onClick={() => setSelectedJoinType(3)} disabled={false}>
+                                        <Attendee userId={'none'}>視聴する</Attendee>
+                                    </div>
+                                )
+                        ) : (
+                            <div className={`${styles.attendees} ${styles.disable}`} onClick={() => setSelectedJoinType(3)} disabled={true}>
+                                <Attendee userId={'none'}>視聴する</Attendee>
+                            </div>
+                        )
+                }
             </div>
             <div className={styles.buttonSecssion}>
                 {
